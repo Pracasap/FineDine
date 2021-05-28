6a07b7924703e480707401e64a60d701b818c49a
Prakash Sapkota <optimistic.prakash@gmail.com>
2021-05-28 13:15:20 -0500

    Added uglify and minify on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 // Define selector for selecting
-        // anchor links with the hash
-        let anchorSelector = 'a[href^="#"]';
+// anchor links with the hash
+let anchorSelector = 'a[href^="#"]';
 
-        // Collect all such anchor links
-        let anchorList =
-            document.querySelectorAll(anchorSelector);
+// Collect all such anchor links
+let anchorList =
+    document.querySelectorAll(anchorSelector);
 
-        // Iterate through each of the links
-        anchorList.forEach(link => {
-            link.onclick = function (e) {
+// Iterate through each of the links
+anchorList.forEach(link => {
+    link.onclick = function (e) {
 
-                // Prevent scrolling if the
-                // hash value is blank
-                e.preventDefault();
+        // Prevent scrolling if the
+        // hash value is blank
+        e.preventDefault();
 
-                // Get the destination to scroll to
-                // using the hash property
-                let destination =
-                    document.querySelector(this.hash);
+        // Get the destination to scroll to
+        // using the hash property
+        let destination =
+            document.querySelector(this.hash);
 
-                // Scroll to the destination using
-                // scrollIntoView method
-                destination.scrollIntoView({
-                    behavior: 'smooth'
-                });
-            }
+        // Scroll to the destination using
+        // scrollIntoView method
+        destination.scrollIntoView({
+            behavior: 'smooth'
         });
+    }
+});
